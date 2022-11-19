ffce31d10f710d00e3c984ccbd96b740ccc9c26b
adnandedhawala <adnan.dedhawala@gmail.com>
2022-11-19 08:58:52 +0530

    Update index.js

--- Week-6/6.4/index.js
+++ Week-6/6.4/index.js
@@ -1,20 +1,13 @@
 const getMaxProfit = arr => {
   let min = arr[0];
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < min) {
-      min = arr[i];
-    }
-  }
-  let start = arr.indexOf(min);
-  let maxProfit = 0;
-  while (start < arr.length) {
-    if (arr[start] - min > maxProfit) {
-      maxProfit = arr[start] - min;
-    }
-    start++;
+  let maxProfit=0;
+  for(let i=1;i<arr.length;i++){
+    let currentProfit = arr[i]-min;
+    if(currentProfit>maxProfit)maxProfit=currentProfit;
+    if(arr[i]<min)min=arr[i]
   }
 
   return maxProfit;
 };
 
-console.log(getMaxProfit([7, 6, 4, 3, 1]));
+console.log(getMaxProfit([7,6,4,3,1]));
